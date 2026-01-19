2f911931f6547576f0113a81bfd483b67ae77d64
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2026-01-19 13:07:58 +0500

    add gitignore and remove ignored files

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Clinic API running ✅"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
